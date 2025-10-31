dfa1d0c64602b1eddfeffc1bef00088eef26599e
Pedro Andreotti <160793937+Pedro-Andreotti@users.noreply.github.com>
2025-10-31 00:44:15 +0000

    feat adiciona o post do Produto

--- projeto-quarkus/src/main/java/recursos/ProdutoRecurso.java
+++ projeto-quarkus/src/main/java/recursos/ProdutoRecurso.java
@@ -3,13 +3,21 @@ package recursos;
 import java.util.List;
 
 import entities.Produto;
+import jakarta.transaction.Transactional;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 
-@Path("produtos")
+@Path("/produtos")
 public class ProdutoRecurso {
     @GET
-    public List<Produto> listar(){
+    public List<Produto> listar() {
         return Produto.listAll();
+    }
+
+    @POST
+    @Transactional
+    public void salvar(Produto produto) {
+        produto.persist();
     }
 }
